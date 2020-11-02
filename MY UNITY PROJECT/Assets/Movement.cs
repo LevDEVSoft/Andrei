20d5739b2578450bee6ecc5f616387301c7999b0
SamsungiphonePro <73790749+SamsungiphonePro@users.noreply.github.com>
2020-11-01 18:09:57 -0800

    Version 0.0.1

---
+++ MY UNITY PROJECT/Assets/Movement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float scalemax;
+    public float scalemin;
+    public float maxOrbitSpeed;
+    float orbitSpeed;
+    public Transform orbitAnchor;
+    Vector3 OrbitDirection;
+    Vector3 maxScale;
+    public float GrowingSpeed;
+    private bool IsScaled = false;
+
+
+    
+    void CubeSettings()
+    {
+        float x = Random.Range(-1f, 1f);
+        float z = Random.Range(-1f, 1f);
+        float y = Random.Range(-1f, 1f);
+        OrbitDirection = new Vector3(x, y, z);
+        orbitSpeed = Random.Range(0.5f, maxOrbitSpeed);
+        float scale = Random.Range(scalemin, scalemax);
+        transform.localScale = Vector3.zero;
+        maxScale = new Vector3(scale, scale, scale);
+
+
+
+
+    }
+    void Start()
+        {
+            CubeSettings();
+
+        }
+    private void Update()
+        {
+        RotateCube();
+            if(!IsScaled)
+            {
+                ScaleObject();
+            }
+
+        }
+    void RotateCube()
+        {
+            transform.RotateAround(orbitAnchor.position, OrbitDirection, orbitSpeed * Time.deltaTime);
+        transform.Rotate(OrbitDirection * 30 * Time.deltaTime);
+
+        }
+  
+
+    void ScaleObject()
+    {
+        if (transform.localScale != maxScale)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, maxScale, Time.deltaTime * GrowingSpeed);
+            IsScaled = true;
+        }
+    }
+
+
+}
+
---
+++ MY UNITY PROJECT/Assets/Spawnerscript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public int Totalspawnee;
+    public float timeToSpawn;
+    public GameObject spawnee;
+    public GameObject[] SpawneeList;
+    private bool positionSet;
+    private IEnumeratot Spawnloop()
+    {
+        StartCorountine(Change.Position());
+        yield return new WaitForSeconds(0, 2f);
+        int i = 0;
+        while(i < Totalspawnee - 1)
+        {
+            SpawneeList[i];
+        }
+    }
+
+
+
+
+
+    private GameObject SpawnElement()
+    {
+        GameObject spawnobject = Instiate(spawnee, (Random.insideUnitSphere * 4) + transform.rotation, transform.ratation) as gameObject;
+        float scale = Random.Range(0.5f, 2f);
+        spawnobject.transform.localScale = new Vector3(scale, scale, scale);
+        return spawnobject;
+    }
+}
